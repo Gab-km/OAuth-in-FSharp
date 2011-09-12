d483c4df73b4ab0ffa2f75daed8a9680921bc9de
Gab_km <the25thcromosome@gmail.com>
2011-09-13 00:31:51 +0900

    keyValue関数を作り、OAuthParameterをイコールで連結した。

--- OAuth.Scenario/OAuthScenario.fs
+++ OAuth.Scenario/OAuthScenario.fs
@@ -9,3 +9,10 @@ let ``OAuthパラメータを作る`` () =
     Given (OAuth.parameterize "oauth_nonce" "1111")
     |> It should equal (OAuthParameter ("oauth_nonce", "1111"))
     |> Verify
+
+[<Scenario>]
+let ``OAuthパラメータをKeyValue形式の文字列に変換する`` () =
+    Given (OAuth.parameterize "oauth_nonce" "1111")
+    |> When OAuth.keyValue
+    |> It should equal "oauth_nonce=1111"
+    |> Verify
--- OAuth/OAuth.fs
+++ OAuth/OAuth.fs
@@ -3,3 +3,7 @@
 type OAuthParameter = OAuthParameter of string * string
 
 let parameterize key value = OAuthParameter (key, value)
+
+let keyValue oParam =
+    match oParam with
+    | OAuthParameter (key, value) -> key + "=" + value
