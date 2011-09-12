a5de7df882da7e4de1c7390aa48a6d79bcd44447
Gab_km <the25thcromosome@gmail.com>
2011-09-13 00:42:19 +0900

    parameterizeManyを追加。

--- OAuth.Scenario/OAuthScenario.fs
+++ OAuth.Scenario/OAuthScenario.fs
@@ -16,3 +16,14 @@ let ``OAuthパラメータをKeyValue形式の文字列に変換する`` () =
     |> When OAuth.keyValue
     |> It should equal "oauth_nonce=1111"
     |> Verify
+
+[<Scenario>]
+let ``複数のOAuthパラメータを一度に作る`` () =
+    Given [("oauth_consumer_key", "XXXX");
+            ("oauth_nonce", "1111");
+            ("oauth_signature", "YYYY")]
+    |> When OAuth.parameterizeMany
+    |> It should equal [OAuthParameter ("oauth_consumer_key", "XXXX");
+                        OAuthParameter ("oauth_nonce", "1111");
+                        OAuthParameter ("oauth_signature", "YYYY")]
+    |> Verify
--- OAuth/OAuth.fs
+++ OAuth/OAuth.fs
@@ -4,6 +4,8 @@ type OAuthParameter = OAuthParameter of string * string
 
 let parameterize key value = OAuthParameter (key, value)
 
+let parameterizeMany kvList = List.map (fun (key, value) -> parameterize key value) kvList
+
 let keyValue oParam =
     match oParam with
     | OAuthParameter (key, value) -> key + "=" + value
