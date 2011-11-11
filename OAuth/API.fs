3c54c9fd30380dc9995074a9b5d9ec1c871dc671
Gab_km <the25thcromosome@gmail.com>
2011-11-11 21:31:08 +0900

    add OAuth/API.fsi .

--- OAuth/API.fs
+++ OAuth/API.fs
@@ -1,68 +1,69 @@
-module OAuth.API
+namespace OAuth
 
-open OAuth.Types
-open OAuth.Base
-open OAuth.ExtendedWebClient
+module API =
+    open OAuth.Types
+    open OAuth.Base
+    open OAuth.ExtendedWebClient
 
-let getRequestToken target httpMethod consumerInfo =
-    async {
-        let wc = new System.Net.WebClient ()
-        let url = System.Uri (target)
-        let meth = getHttpMethodString httpMethod
-        let! result =
-            match httpMethod with
-            | GET ->
-                let header = generateAuthorizationHeaderForRequestToken target meth consumerInfo
-                wc.Headers.Add ("Authorization", header)
-                wc.AsyncDownloadString url
-            | POST ->
-                let header = generateAuthorizationHeaderForRequestToken target meth consumerInfo
-                wc.Headers.Add ("Authorization", header)
-                wc.AsyncUploadString url meth ""
-        return result
-    } |> Async.RunSynchronously
+    let getRequestToken target httpMethod consumerInfo =
+        async {
+            let wc = new System.Net.WebClient ()
+            let url = System.Uri (target)
+            let meth = getHttpMethodString httpMethod
+            let! result =
+                match httpMethod with
+                | GET ->
+                    let header = generateAuthorizationHeaderForRequestToken target meth consumerInfo
+                    wc.Headers.Add ("Authorization", header)
+                    wc.AsyncDownloadString url
+                | POST ->
+                    let header = generateAuthorizationHeaderForRequestToken target meth consumerInfo
+                    wc.Headers.Add ("Authorization", header)
+                    wc.AsyncUploadString url meth ""
+            return result
+        } |> Async.RunSynchronously
 
-let getRequestTokenByGet target consumerInfo = getRequestToken target GET consumerInfo
-let getRequestTokenByPost target consumerInfo = getRequestToken target POST consumerInfo
+    let getRequestTokenByGet target consumerInfo = getRequestToken target GET consumerInfo
+    let getRequestTokenByPost target consumerInfo = getRequestToken target POST consumerInfo
 
-let getAccessToken target httpMethod consumerInfo requestInfo pinCode =
-    async {
-        let wc = new System.Net.WebClient ()
-        let url = System.Uri (target)
-        let meth = getHttpMethodString httpMethod
-        let! result =
-            match httpMethod with
-            | GET ->
-                let header = generateAuthorizationHeaderForAccessToken target meth consumerInfo requestInfo pinCode
-                wc.Headers.Add ("Authorization", header)
-                wc.AsyncDownloadString url
-            | POST ->
-                let header = generateAuthorizationHeaderForAccessToken target meth consumerInfo requestInfo pinCode
-                wc.Headers.Add ("Authorization", header)
-                wc.AsyncUploadString url meth ""
-        return result
-    } |> Async.RunSynchronously
+    let getAccessToken target httpMethod consumerInfo requestInfo pinCode =
+        async {
+            let wc = new System.Net.WebClient ()
+            let url = System.Uri (target)
+            let meth = getHttpMethodString httpMethod
+            let! result =
+                match httpMethod with
+                | GET ->
+                    let header = generateAuthorizationHeaderForAccessToken target meth consumerInfo requestInfo pinCode
+                    wc.Headers.Add ("Authorization", header)
+                    wc.AsyncDownloadString url
+                | POST ->
+                    let header = generateAuthorizationHeaderForAccessToken target meth consumerInfo requestInfo pinCode
+                    wc.Headers.Add ("Authorization", header)
+                    wc.AsyncUploadString url meth ""
+            return result
+        } |> Async.RunSynchronously
 
-let getAccessTokenByGet target consumerInfo requestInfo pinCode = getAccessToken target GET consumerInfo requestInfo pinCode
-let getAccessTokenByPost target consumerInfo requestInfo pinCode = getAccessToken target POST consumerInfo requestInfo pinCode
+    let getAccessTokenByGet target consumerInfo requestInfo pinCode = getAccessToken target GET consumerInfo requestInfo pinCode
+    let getAccessTokenByPost target consumerInfo requestInfo pinCode = getAccessToken target POST consumerInfo requestInfo pinCode
 
-let useWebService target httpMethod consumerInfo accessInfo =
-    async {
-        let wc = new System.Net.WebClient ()
-        let url = System.Uri (target)
-        let meth = getHttpMethodString httpMethod
-        let! result =
-            match httpMethod with
-            | GET ->
-                let header = generateAuthorizationHeaderForWebService target meth consumerInfo accessInfo
-                wc.Headers.Add ("Authorization", header)
-                wc.AsyncDownloadString url
-            | POST ->
-                let header = generateAuthorizationHeaderForWebService target meth consumerInfo accessInfo
-                wc.Headers.Add ("Authorization", header)
-                wc.AsyncUploadString url meth ""
-        return result
-    } |> Async.RunSynchronously
+    let useWebService target httpMethod consumerInfo accessInfo =
+        async {
+            let wc = new System.Net.WebClient ()
+            let url = System.Uri (target)
+            let meth = getHttpMethodString httpMethod
+            let! result =
+                match httpMethod with
+                | GET ->
+                    let header = generateAuthorizationHeaderForWebService target meth consumerInfo accessInfo
+                    wc.Headers.Add ("Authorization", header)
+                    wc.AsyncDownloadString url
+                | POST ->
+                    let header = generateAuthorizationHeaderForWebService target meth consumerInfo accessInfo
+                    wc.Headers.Add ("Authorization", header)
+                    wc.AsyncUploadString url meth ""
+            return result
+        } |> Async.RunSynchronously
 
-let useWebServiceByGet target consumerInfo accessInfo = useWebService target GET consumerInfo accessInfo
-let useWebServiceByPost target consumerInfo accessInfo = useWebService target POST consumerInfo accessInfo
+    let useWebServiceByGet target consumerInfo accessInfo = useWebService target GET consumerInfo accessInfo
+    let useWebServiceByPost target consumerInfo accessInfo = useWebService target POST consumerInfo accessInfo
---
+++ OAuth/API.fsi
@@ -0,0 +1,24 @@
+﻿namespace OAuth
+
+module API = begin
+    open OAuth.Utilities
+    open OAuth.Types
+
+    val getRequestToken : string -> HttpMethod -> ConsumerInfo -> string
+
+    val getRequestTokenByGet : string -> ConsumerInfo -> string
+
+    val getRequestTokenByPost : string -> ConsumerInfo -> string
+
+    val getAccessToken : string -> HttpMethod -> ConsumerInfo -> RequestInfo -> string -> string
+
+    val getAccessTokenByGet : string -> ConsumerInfo -> RequestInfo -> string -> string
+
+    val getAccessTokenByPost : string -> ConsumerInfo -> RequestInfo -> string -> string
+
+    val useWebService : string -> HttpMethod -> ConsumerInfo -> AccessInfo -> string
+
+    val useWebServiceByGet : string -> ConsumerInfo -> AccessInfo -> string
+
+    val useWebServiceByPost : string -> ConsumerInfo -> AccessInfo -> string
+end
